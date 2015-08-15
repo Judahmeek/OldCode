60b89b2a86d50be82030be36b66353d3919d508e
Judahmeek <judah.meek@gmail.com>
2015-08-15 02:46:46 -0700

    Max Words Program
    Finds the maximum number of words (defined as at least one letter
    separated from other words by spaces and/or punctuation) in a sentence,
    which is separated from other sentences by punctuation.

---
+++ Python 3/Max Words.py
@@ -0,0 +1,28 @@
+import string
+
+def solution(string):
+    max = 0
+    words = 0
+    space = False
+    first = True
+    for i in range(len(string)):
+        if string[i].isspace():
+            space = True
+        elif string[i].isalpha() and space:
+            words = words + 1
+            space = False
+			if first:
+				first = False
+        elif string[i].isalpha() and first:
+            words = words + 1
+            first = False
+        elif not (string[i].isalpha() or string[i].isspace()):
+            if words > max:
+                max = words
+            words = 0
+			first = True
+    if words > max:
+        max = words
+    return max
+	
+#https://codility.com/c/feedback/X6ASAS-XYK

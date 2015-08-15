4aae44e888eef92f310705a88fef414f4b6c07df
Judahmeek <judah.meek@gmail.com>
2015-08-15 03:00:27 -0700

    Change to rightDigit value assignment conditions
    Removing if(*rightDigitPtr <= *leftDigitPtr) resets rightDigit anytime
    leftDigit is incremented this function, which solves a glitch we had
    where rightDigit would increasingly raise in value, invalidating swap
    comparions, and leading to an out-of-bounds error.
    
    Placing the other rightDigit assignment within an else clause was simply
    to save some processing cycles.

--- C/Plaindrome/scanSwapRecord.c
+++ C/Plaindrome/scanSwapRecord.c
@@ -5,12 +5,13 @@
 
 void scanSwapRecord(swap swapRow[], short* swapRecordIndexPtr, short bufferSignatureRow[], short* leftDigitPtr, short* rightDigitPtr, short tscs){
 	while(swapRow[*swapRecordIndexPtr].index != -1 && *swapRecordIndexPtr < tscs){
-		*rightDigitPtr = swapRow[*swapRecordIndexPtr].index + 1;
 		++*swapRecordIndexPtr;
-		if(*swapRecordIndexPtr > bufferSignatureRow[*leftDigitPtr])
+		if(*swapRecordIndexPtr > bufferSignatureRow[*leftDigitPtr]){
 			++*leftDigitPtr;
-		if(*rightDigitPtr <= *leftDigitPtr)
 			*rightDigitPtr = *leftDigitPtr + 1;
+		} else {
+			*rightDigitPtr = swapRow[*swapRecordIndexPtr].index + 1;
+		}
 	}
 }
 

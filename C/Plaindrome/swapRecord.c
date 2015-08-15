c32122fdd18633fad665dba7d26f2364278dfaad
Judahmeek <judah.meek@gmail.com>
2015-08-15 03:06:55 -0700

    DEBUG Proprocessor Directives
    I've gotten tired of creating and erasing basic debug output code. This
    time I'm going to try a new trick, which I may then be adding to my
    arsenal if I deem it effective.

--- C/Plaindrome/swapRecord.c
+++ C/Plaindrome/swapRecord.c
@@ -1,3 +1,6 @@
+#ifdef DEBUG
+#include <stdio.h>
+#endif
 #include <stdlib.h>
 #include "state2steps.c"
 #include "scanSwapRecord.c"
@@ -36,10 +39,22 @@ swap** swapRecord(const short freq[], short freqSize, short stateSize, int total
 	
     	leftDigit = 0, rightDigit = 1, swapRecordIndex = 0;
     	
+    	#ifdef DEBUG
+    	printf("\nstateID: %d\n", stateID);
+    	printTitledSwapArray("swapRecord: ", swapRecord[stateID], tscs, 1);
+    	scanSwapRecord(swapRecord[stateID], &swapRecordIndex, bufferSig[stateID], &leftDigit, &rightDigit, tscs);
+    	printf("scan moves leftDigit to %d and rightDigit to %d\n", leftDigit, rightDigit);
+    	#endif
     	
     	while(swapRecordIndex < tscs){
+    		#ifdef DEBUG
+    		printf("\nswapRecordIndex: %d tscs: %d\n", swapRecordIndex, tscs);
+    		#endif
     		if(states[stateID][leftDigit] == states[stateID][rightDigit]){
     			++rightDigit;
+    			#ifdef DEBUG
+    			puts("leftDigit and rightDigit same. rD increments by one.");
+    			#endif
 			}
     		else{
     			for(j = 0; j < stateSize; ++j) //initialize swapState
@@ -47,6 +62,16 @@ swap** swapRecord(const short freq[], short freqSize, short stateSize, int total
     			
     			swapState[leftDigit] = states[stateID][rightDigit]; //perform swap
     			swapState[rightDigit] = states[stateID][leftDigit];
+    			
+    			#ifdef DEBUG
+    			if(arraySum(swapState, stateSize) != 6){
+    				printf("leftDigit: %d, rightDigit: %d\n", leftDigit, rightDigit);
+	    			printTitledShortArray("baseState: ", states[stateID], stateSize, 0);
+	    			printf(", stateSize: %d stateSum: %d\n", stateSize, arraySum(states[stateID], stateSize));		
+	    			printTitledShortArray("swapState: ", swapState, stateSize, 0);
+	    			printf(", stateSize: %d stateSum: %d\n", stateSize, arraySum(swapState, stateSize));
+				}
+				#endif
     			
     			swapStateBuffer = 0;
     			for(j = leftDigit + 1; j < rightDigit; ++j)
@@ -58,12 +83,29 @@ swap** swapRecord(const short freq[], short freqSize, short stateSize, int total
     			insertSwapLink(&swapRecord[stateID][swapRecordIndex], partnerID, &swapRecord[partnerID][bufferSig[partnerID][leftDigit] + swapStateBuffer], stateID, rightDigit);
     			
     			++swapRecordIndex, ++rightDigit;
+    			#ifdef DEBUG
+    			printf("\nSRI incremented to %d\n", swapRecordIndex);
+    			#endif
     			scanSwapRecord(swapRecord[stateID], &swapRecordIndex, bufferSig[stateID], &leftDigit, &rightDigit, tscs);
+    			#ifdef DEBUG
+    			printf("scan moves leftDigit to %d and rightDigit to %d\n", leftDigit, rightDigit);
+    			#endif
 			}
+			#ifdef DEBUG
+			printf("\nleftDigit: %d, rightDigit: %d, stateSize: %d\n", leftDigit, rightDigit, stateSize);
+			#endif
 			if(rightDigit >= stateSize){
 				++leftDigit;
 				rightDigit = leftDigit + 1;
+				#ifdef DEBUG
+    			printf("leftDigit changed to %d and rightDigit to %d\n", leftDigit, rightDigit);
+    			#endif
 			}
+		#ifdef DEBUG
+		printf("swapRecord[%d]: ", stateID);
+		printSwapArray(swapRecord[stateID], tscs);
+		putchar('\n');
+		#endif
 		}
 	}
 	return swapRecord;

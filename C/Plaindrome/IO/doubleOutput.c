d9255be5dd2c1b3bd91c1b5477c83c505eb6c8b6
Judahmeek <judah.meek@gmail.com>
2015-08-27 12:42:26 -0700

    doubleOutput() improved
    I'd love to say that it was perfected, but I'm sure there's something
    that could still be improved. I'm proud of what I achieved though.

--- C/Plaindrome/IO/doubleOutput.c
+++ C/Plaindrome/IO/doubleOutput.c
@@ -7,25 +7,39 @@ void doubleOutput(double num, short precision){
 	
 	if(num){
 		
-		--precision;
-		int multiplier = 10;
+		++precision;
 		
-		while(precision){
-			while(precision % 2 == 0){
-				precision /= 2;
-				multiplier *= multiplier;
-			}
-			multiplier *= 10;
-			--precision;
-		}
-	
 		if(num < 0){
 			putchar('-');
 			num *= -1;
 		}
 		intOutput((int)num);
 		putchar('.');
-		intOutput((int)((num - (int)num) * multiplier));
+		
+		double remainder = (num - (int)num);
+		num -= (int)num;
+		int decimal = 0;
+		while(precision){
+			num *= 10;
+			decimal = decimal * 10 + (int)num;
+			printf("\ndecimal: %d\n", decimal);
+			num -= (int)num;
+			--precision;
+		}
+		if(decimal){
+			if(decimal % 10 > 4)
+				decimal += 10;
+			else
+				decimal -= 10;
+			decimal /= 10;
+		}
+		if(!decimal){
+			while(precision){
+			putchar('0');
+			--precision;
+			}
+		}
+		intOutput(decimal);
 	}
 	else{
 		putchar('0');

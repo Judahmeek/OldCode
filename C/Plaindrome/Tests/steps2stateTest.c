0407c982309e152c53a348e7203cd660f22ff54d
Judahmeek <judah.meek@gmail.com>
2015-09-10 19:51:22 -0700

    Added nonexhaustive steps2state test mode

--- C/Plaindrome/Tests/steps2stateTest.c
+++ C/Plaindrome/Tests/steps2stateTest.c
@@ -1,15 +1,18 @@
+#define EXHAUSTIVESTEPS2STATEDEBUG
+//#define DEBUGSTEPS2STATE
 #include "../IO/printTitledShortArray.c"
 #include "../digitSize_i.c"
 #include "../arraySum.c"
 #include "../steps2state.c"
 
 int main() {
-	const short freq[] = {2, 2, 1};
-	short freqSize = 3;
+	const short freq[] = {2, 2, 2, 1};
+	short freqSize = 4;
 	short stateSize = arraySum(freq, freqSize);
 	int uBound = factorial(stateSize)/arrayFactorial(freq, freqSize, 0, 0);
 	short result[stateSize];
 	short input = 0, width = digitSize_i(uBound);
+	#ifdef EXHAUSTIVESTEPS2STATEDEBUG
 	int steps;
 	for(steps = 0;steps < uBound; ++steps){
 		printf("Steps: %*d -> ", width, steps);
@@ -22,5 +25,10 @@ int main() {
     		input = 0;
     	}
 	}
+	#else //EXHAUSTIVESTEPS2STATEDEBUG
+	int steps = 44;
+	steps2state(freq, freqSize, result, stateSize, steps);
+	printTitledShortArray("Result: ", result, stateSize, 1);
+	#endif //EXHAUSTIVESTEPS2STATEDEBUG
 	return 0;
 }

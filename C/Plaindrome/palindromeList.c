d1ab3beaf57b378c0250bbea202525e6375df42b
Judahmeek <judah.meek@gmail.com>
2015-09-10 21:04:32 -0700

    palindromeList() created

---
+++ C/Plaindrome/palindromeList.c
@@ -0,0 +1,38 @@
+#ifdef DEBUGPALINDROMELIST
+#include "./IO/printShortArray.c"
+#endif
+
+#include <stdlib.h>
+#include "state2steps.c"
+
+int* palindromeList(const short freq[], short freqHalf[], short freqSize, short stateSize, short numPalindrome){
+	int* list = (int*)malloc(numPalindrome * sizeof(int));
+	
+	if(stateSize == 1)
+		*list = 0;
+	else{
+		short i, j, halfSize = stateSize / 2, size = stateSize - 1;
+		short state[stateSize];
+		if(stateSize % 2 != 0){
+			state[halfSize] = freqSize - 1;
+		}
+		
+		for(i = 0; i < numPalindrome; ++i){
+			
+			steps2state(freqHalf, freqSize, state, halfSize, i);
+			for(j = 0; j < halfSize; ++j){
+				state[size - j] = state[j];
+			}
+			
+			#ifdef DEBUGPALINDROMELIST
+			printf("state[%d]: ", i);
+			printShortArray(state, stateSize);
+			putchar('\n');
+			#endif
+			
+			list[i] = state2steps(freq, freqSize, state, stateSize);
+		}
+	}
+	
+	return list;
+}

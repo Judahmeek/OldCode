4779f71488aab0124c8c3b455985a75c63c2e21e
Judahmeek <judah.meek@gmail.com>
2015-09-10 21:17:37 -0700

    initializeMatrix() modified

--- C/Plaindrome/InitializeMatrix.c
+++ C/Plaindrome/InitializeMatrix.c
@@ -1,11 +1,15 @@
-#include "initializeSwapRecord.c"
-#include "./IO/displayMatrix.c"
-#include "./IO/printIntArray.c"
-#ifdef DEBUG
+#ifdef DEBUGINITIALIZEMATRIX
 #include "./IO/systemPause.c"
 #endif
+#include "initializeSwapRecord.c"
+#include "./IO/printDoubleArrayAs2D.c"
+#include "./IO/printIntArray.c"
+#include "findMatrixRow.c"
 
-double ** initializeMatrix(const short freq[], short freqSize, short stateSize, int totalStates, int tscs, int posSwaps){
+/*	To save time, the markov matrix is initiated to I - Q, where I is the identity matrix of the transition matrix Q
+*/
+
+double ** initializeMatrix(int** swapRecord, int totalStates, int tscs, int* palList, short numPalindrome, int posSwaps){
 	
 	int i;
 	int swapsThatMaintainState = (posSwaps - tscs);
@@ -18,39 +22,48 @@ double ** initializeMatrix(const short freq[], short freqSize, short stateSize, 
 	printf("1 / posSwaps: %f\n", singleSwapsPercentageOfPosSwaps);
 	#endif
 	
-	double** markovMatrix = (double **)calloc(totalStates, sizeof(double *));
-    markovMatrix[0] = (double *)calloc(totalStates * totalStates, sizeof(double));
-    for(i = 0; i < totalStates; ++i)
-        markovMatrix[i] = (*markovMatrix + totalStates * i);
+	int transitionMatrixSize = totalStates - numPalindrome;
+	double** markovMatrix = (double **)malloc(transitionMatrixSize * sizeof(double *));
+    markovMatrix[0] = (double *)calloc(transitionMatrixSize * transitionMatrixSize, sizeof(double));
+    for(i = 0; i < transitionMatrixSize; ++i)
+        markovMatrix[i] = (markovMatrix[0] + transitionMatrixSize * i);
 	
-	int** swapRecord = initializeSwapRecord(freq, freqSize, stateSize, totalStates, tscs);
-    
-    #ifdef DEBUG
-	for(i = 0; i < totalStates; ++i){
-		printf("swapRecord[%d]: ", i);
-		printIntArray(swapRecord[i], tscs);
-		putchar('\n');
-	}
-	systemPause();
-	#endif
-	
+	int j = 0;
 	short swapRecordIndex;
-	for(i = 0; i < totalStates; ++i){
-        markovMatrix[i][i] = stmsPercentageOfPosSwaps;
-        for(swapRecordIndex = 0; swapRecordIndex < tscs; ++swapRecordIndex){
-        	#ifdef DEBUG
-        	if(i > -1 && i < 1)
-        		printf("markovMatrix[%d][swapRecord[%d][%d]: %d] = %f;\n", i, i, swapRecordIndex, swapRecord[i][swapRecordIndex], singleSwapsPercentageOfPosSwaps);
-        	#endif
-        	markovMatrix[i][swapRecord[i][swapRecordIndex]] = singleSwapsPercentageOfPosSwaps;
+	for(i = 0; i < transitionMatrixSize; ++i, ++j){
+    	
+        while(swapRecord[j][0] == -tscs) //if state == palindrome
+        	++j; //move to the next state
+        
+        markovMatrix[i][findMatrixRow(palList, numPalindrome, j)] = stmsPercentageOfPosSwaps;
+    	#ifdef DEBUGINITIALIZEMATRIX
+    	if(i == 9) //bounds for limiting what is displayed
+        	printf("markovMatrix[%d]findMatrixRow(palList, numPalindrome, %d): %d] = %f;\n", i, j, findMatrixRow(palList, numPalindrome, j), stmsPercentageOfPosSwaps);
+    	#endif
+        
+		if(swapRecord[j][0] < 0)
+			swapRecordIndex = -swapRecord[j][0];
+		else
+			swapRecordIndex = 0;
+		
+        for(; swapRecordIndex < tscs; ++swapRecordIndex){
+        	#ifdef DEBUGINITIALIZEMATRIX
+        	if(i == 9){ //bounds for limiting what is displayed
+				printf("swapRecord[%d]: ", j);
+				printIntArray(swapRecord[j], tscs);
+				putchar('\n');
+        		printf("markovMatrix[%d]findMatrixRow(palList, numPalindrome, [swapRecord[%d][%d]): %d] = %f;\n", i, j, swapRecordIndex, findMatrixRow(palList, numPalindrome, swapRecord[j][swapRecordIndex]), singleSwapsPercentageOfPosSwaps);
+        	}
+			#endif
+        	markovMatrix[i][findMatrixRow(palList, numPalindrome, swapRecord[j][swapRecordIndex])] = singleSwapsPercentageOfPosSwaps;
 		}
-	#ifdef DEBUG
+	#ifdef DEBUGINITIALIZEMATRIX
 	//systemPause();
 	#endif
 	}
 	
-	#ifdef BASICS
-	displayMatrix(markovMatrix[0], totalStates, 2);
+	#ifdef DEBUGINITIALIZEMATRIX
+	printDoubleArrayAs2D("Markov Matrix: ", markovMatrix[0], transitionMatrixSize, transitionMatrixSize, 2);
 	#endif
 	
 	return markovMatrix;
